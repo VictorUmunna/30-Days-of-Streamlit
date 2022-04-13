be3e677fa495c3f54c2fa1654d0fdc684261cfeb
Victor Umunna <umunnavictor01@gmail.com>
2022-04-13 23:49:56 +0100

    update app

--- Day_10/selectbox.py
+++ Day_10/selectbox.py
@@ -4,6 +4,7 @@ st.header('st.selectbox')
 
 option = st.selectbox(
     'Who is your favourite superhero?',
-    ('Ironman', 'Hulk', 'Thor', 'Captain America', 'Spiderman', 'Black Panther', 'Black Widow', 'Ant')
+    ('Ironman', 'Hulk', 'Thor', 'Captain America', 'Spiderman',
+     'Black Panther', 'Black Widow', 'Ant')
 )
 st.write('Your favourite superhero is:', option)
