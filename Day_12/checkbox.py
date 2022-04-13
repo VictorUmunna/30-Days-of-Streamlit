fdf25bf45b6dfd1fc4846938600a779bf45eb755
Victor Umunna <umunnavictor01@gmail.com>
2022-04-14 00:29:32 +0100

    build checkbox app

---
+++ Day_12/checkbox.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+boli = st.checkbox('Boli')
+guguru = st.checkbox('Guguru')
+nsala = st.checkbox('Nsala')
+
+if boli:
+    st.write('Great! Here is your Boli')
+
+if guguru:
+    st.write("Okay Here's some Guguru")
+
+if nsala:
+    st.write("Enjoy youur Nsala")
