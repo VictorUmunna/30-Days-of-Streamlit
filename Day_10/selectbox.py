ba26a9fc3cd51d1c40cdfa0678ce6a81f4465610
Victor Umunna <umunnavictor01@gmail.com>
2022-04-13 23:31:36 +0100

    build selectbox app

---
+++ Day_10/selectbox.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+    'Who is your favourite superhero?',
+    ('Ironman', 'Hulk', 'Thor', 'Captain America', 'Spiderman', 'Black Panther', 'Black Widow', 'Ant')
+)
+st.write('Your favourite superhero is:', option)
